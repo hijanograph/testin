6e5b3bad536d93d3cd21cb444d2195a5fdce415f
hijanograph <hijano.graph@gmail.com>
2026-03-20 12:42:00 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ export default defineConfig({
   // Cambia esto por tu usuario y nombre de repo de GitHub:
   // Si el repo se llama "rafael-hijano" → site: 'https://tuusuario.github.io', base: '/'
   // Si el repo se llama "portfolio" → site: 'https://tuusuario.github.io', base: '/portfolio'
-  site: 'https://rafaelhijano.github.io',
-  base: '/',
+  site: 'https://hijanograph.github.io',
+  base: '/testin',
   output: 'static',
 });
