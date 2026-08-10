3a344b46290c2d727b8b59a2df3c394c9af06b94
hijanograph <hijano.graph@gmail.com>
2026-08-10 11:24:49 +0200

    Remove CloudCannon integration
    Removed CloudCannon integration from Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
-import cloudcannon from '@cloudcannon/astro-connector';
 
 export default defineConfig({
   site: 'https://rafaelhijano.work',
-  integrations: [
-    cloudcannon(),
-  ],
 });
